06886662a04e13fa7b4951cafd60d4e757a49def
jher <tim668666@gmail.com>
2023-05-14 22:43:47 +0900

    second commit

--- Weatherapp/App.js
+++ Weatherapp/App.js
@@ -1,39 +1,109 @@
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, Dimensions} from 'react-native';
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const [city, setCity] = useState("Loading...")
+  
+  useEffect(() => {
+    ask();
+  },[])
+
+  const ask= async()=>{
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if(!granted){
+      setOk(false);
+    }
+    // const location = await Location.getCurrentPositionAsync({accuracy:5});
+   const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5});
+   const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps:false});
+    setCity(location[0].city)
+  }
   return (
-    <View style={styles.container
-    }>
-      <Text style={testStyle.first}>hi! jher!</Text>
-   
-      <StatusBar style="dark" />
-    </View>
+    <View style={mystyle.container}>
+      <View style={mystyle.city}>
+      <Text style={mystyle.cityname}>{city}</Text>
+      </View>
+
+      <ScrollView showsHorizontalScrollIndicator={false} 
+      pagingEnabled 
+      horizontal 
+      contentContainerStyle={mystyle.weather}>
+        <View style={mystyle.day}>
+          <Text style={mystyle.temp}>27</Text>
+          <Text style={mystyle.description}>sunny</Text>
+        </View>
+        <View style={mystyle.day}>
+          <Text style={mystyle.temp}>27</Text>
+          <Text style={mystyle.description}>sunny</Text>
+        </View>
+        <View style={mystyle.day}>
+          <Text style={mystyle.temp}>27</Text>
+          <Text style={mystyle.description}>sunny</Text>
+        </View>
+        <View style={mystyle.day}>
+          <Text style={mystyle.temp}>27</Text>
+          <Text style={mystyle.description}>sunny</Text>
+        </View>
+      </ScrollView>
+      
+
+    <StatusBar style="black"/>
+  </View> 
+ 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "gold",
-    
+const screen_width = Dimensions.get('window').width;
+// const {width:screen_width}=Dimensions.get('window');
+
+
+const mystyle = StyleSheet.create({
+  container:{
+    flex:1, 
+    backgroundColor:"gold",
   },
-  text:{
-    fontSize: 28,
-    
+  city:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+
+  },
+  cityname:{
+  color:"darkblue",
+  fontSize:58,
+  fontWeight:700,
+  },
+  day:{
+    width: screen_width,
+    fontSize:50,
+    alignItems:"center",
+  },
+  temp:{
+      fontSize:128,
+      marginTop:30,
+  },
+  description:{
+    fontSize:60,
+    marginTop:-10
+  },
+  weather:{
+    //backgroundColor:"gray",
   }
 
-});
-const testStyle = {
-  first:{
-    backgroundColor: "white",
-    fontSize:50,
-    color:"blue",
+})
+
+
+// const testStyle = {
+//   first:{
+//     backgroundColor: "white",
+//     fontSize:50,
+//     color:"blue",
     
-  },
+//   },
 
 
-}
+// }
